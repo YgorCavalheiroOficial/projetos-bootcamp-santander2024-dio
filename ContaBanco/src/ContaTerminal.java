69118cb37eb1fad38c43aaa5abfd9374c202d23b
ycavalheiro <ygorcavalheiro93@gmail.com>
2024-05-01 19:24:02 -0300

    Projeto_1

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
 
